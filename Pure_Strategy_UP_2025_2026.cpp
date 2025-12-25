2faf3c8348cfb6d097c7883c0c9c786f713a2b3b
Kremi16 <kremikoleva16@gmail.com>
2025-12-25 16:54:17 +0200

    Main program file for the project
    Create an empty main file with a personal academic description for the console implementation of the card game "Pure Strategy".

--- Pure_Strategy_UP_2025_2026.cpp
+++ Pure_Strategy_UP_2025_2026.cpp
@@ -1,5 +1,20 @@
 ﻿#include <iostream>
 
+/**
+*
+* Solution to course project # 01
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Kremena Koleva
+* @idnumber 9MI0600605
+* @compiler VC
+*
+* Main program file for the console implementation of the card game "Pure strategy".
+*
+*/
+
 int main()
 {
 	return 0;
